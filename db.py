d7d0bdcd197ea28330e094cfd47e9f80a82ac30d
famaya <famaya.jro@gmail.com>
2025-08-21 12:15:55 -0500

    Update db.py

--- db.py
+++ db.py
@@ -1,13 +1,11 @@
 from pymongo import MongoClient
 from config import Config
 
-# Conexión a Mongo Atlas
 client = MongoClient(Config.MONGO_URI)
-
-# Usamos la base de datos "ava"
 db = client["ava"]
 
-# Colecciones en inglés
+# Colecciones
+users = db["users"]        
 students = db["students"]
 attendance = db["attendance"]
 payments = db["payments"]
