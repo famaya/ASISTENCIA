84f8916c2917b06775497c95a8e93203372068ed
famaya <famaya.jro@gmail.com>
2025-09-01 20:27:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,9 @@ from routes.students import students_bp
 from routes.professors import professors_bp
 from routes.groups import groups_bp
 from routes.sedes import sedes_bp
-# Esto lo puedes poner al inicio de app.py
-app.config['VERSION'] = '1.0.0'
+
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+
 app = Flask(__name__)
 
 # Registrar Blueprints
